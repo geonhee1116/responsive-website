dd864d933f0dd853cb828262c23d787dbfd73e57
geonhee1116 <hgh717171@naver.com>
2021-11-03 17:46:55 +0900

    js

--- main.js
+++ main.js
@@ -15,3 +15,27 @@ document.addEventListener("scroll", () => {
         navbar.classList.remove('navbar--dark'); 
     }
 });
+
+//넷바 메뉴 선택시 해당 부분으로 이동하기
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event)=> {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link);
+});
+
+//contact me 버튼 이동하기
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior : 'smooth'});
+}
+
+const contactMe = document.querySelector(".home__contact");
+contactMe,addEventListener("click", () => {
+    scrollIntoView("#contact");
+});
